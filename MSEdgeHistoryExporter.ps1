8da61c0271a8419b49ac61ce12d9a48d9e417e93
longlanky <102842196+longlanky@users.noreply.github.com>
2022-06-27 21:03:18 -0400

    Update MSEdgeHistoryExporter.ps1
    *Added option to generate report for all local users

--- MSEdgeHistoryExporter.ps1
+++ MSEdgeHistoryExporter.ps1
@@ -1,3 +1,6 @@
+
+Function PullData { Invoke-SqliteQuery -Query "SELECT url,title,visit_count,last_visit_time FROM urls" -DataSource "C:\Users\$SelectedUser\AppData\Local\Microsoft\Edge\User Data\Default\history" | Sort-Object last_visit_time -Descending | Select-Object *, @{ Name = 'date_last_accessed'; Expression = { '=TEXT(INDIRECT("D"&ROW())*(1.15740740740741E-11)-(109205.1665),"mm.dd.yyyy HH:mm AM/PM")' } } | Export-Csv -Path "C:\Users\$env:UserName\Downloads\MSEdge_History_$SelectedUser`_$(Get-Date -Format "HHmm_MMddyy").csv" }
+
 Write-Host "Checking for requisite module, this may take a while."
 $Error.clear()
 $ModuleState = Get-InstalledModule -Name "PSSQLite" -ErrorAction SilentlyContinue
@@ -12,7 +15,12 @@ foreach ($name in $LocalUsers) {
 }
 if ($Null -eq $Names) { Write-Host "No users with Edge history found!"; Break }
 Write-Host "History found for following users:`n$Names`r"
-$SelectedUser = Read-Host -Prompt "Enter username to export MS Edge history, or leave blank to export current user ($env:UserName)"
+$SelectedUser = Read-Host -Prompt "Enter username to export MS Edge history, leave blank to export current user ($env:UserName), or enter 'ALL' to export all users"
+If ($SelectedUser -Match "ALL") {
+    if ($null -ne $(Get-Process | Where-Object { $_.ProcessName -Like "*msedge" }) -and $(Test-Path -Path "C:\Users\$env:UserName\AppData\Local\Microsoft\Edge\User Data\Default\history" -PathType Leaf)) { $EdgeIsOpen = Read-Host -Prompt "Close edge, or press enter to close edge and proceed."; If ($null -eq $EdgeIsOpen -or $null -ne $EdgeIsOpen) { Write-Host "Closing Edge"; Get-Process | Where-Object { $_.ProcessName -Like "*msedge*" } | Stop-Process } }
+    Write-Host "Exporting all users to Downloads folder."
+    ForEach ($name in $LocalUsers) { If (Test-Path -Path "C:\Users\$name\AppData\Local\Microsoft\Edge\User Data\Default\history" -PathType Leaf) { $SelectedUser = $name; PullData } }; Break
+}
 If ($null -match $SelectedUser) { $SelectedUser = $env:UserName }
 $SanityCheck = Test-Path -Path "C:\Users\$SelectedUser\AppData\Local\Microsoft\Edge\User Data\Default\history" -PathType Leaf
 If (($env:UserName) -notmatch $SelectedUser -and $SanityCheck -match "True") { Write-Host "Exporting $SelectedUser's history to Downloads folder" }
@@ -26,5 +34,5 @@ Elseif (($env:UserName) -match $SelectedUser -and $SanityCheck -match "True") {
 }
 Elseif ($SanityCheck -match "False") { Write-Host "User history not found, account either not present or history is absent. Exiting."; Break }
 $ExitCheck = "C:\Users\$env:UserName\Downloads\MSEdge_History_$SelectedUser`_$(Get-Date -Format "HHmm_MMddyy").csv"
-Invoke-SqliteQuery -Query "SELECT url,title,visit_count,last_visit_time FROM urls" -DataSource "C:\Users\$SelectedUser\AppData\Local\Microsoft\Edge\User Data\Default\history" | Sort-Object last_visit_time -Descending | Select-Object *, @{ Name = 'date_last_accessed'; Expression = { '=TEXT(INDIRECT("D"&ROW())*(1.15740740740741E-11)-(109205.1665),"mm.dd.yyyy HH:mm AM/PM")' } } | Export-Csv -Path "C:\Users\$env:UserName\Downloads\MSEdge_History_$SelectedUser`_$(Get-Date -Format "HHmm_MMddyy").csv"
+PullData
 If ((Get-Item $ExitCheck).length -lt 1kb) { If ($SelectedUser -match $env:UserName) { Write-Host "Report did not generate, ensure that Edge is closed and retry" } Else { Write-Host "Report did not generate, history may not be present for $SelectedUser." } } 
